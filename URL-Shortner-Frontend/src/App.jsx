a72fb7161d853dc096030f964f5f133d1c7ab2db
aman3255 <amanprajapati3255@gmail.com>
2025-02-04 16:42:22 +0530

    Added URL Shortner Frontend

---
+++ URL-Shortner-Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import ButtonScreen from "./screens/ButtonScreen"
+import LinksScreen from "./screens/LinksScreen"
+
+function App() {
+
+  return (
+    <>
+      {/* <ButtonScreen/> */}
+      <LinksScreen/>
+    </>
+  )
+
+}
+
+export default App
